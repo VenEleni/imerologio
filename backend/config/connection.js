1a721ddab5957e26f42d3cd801efd1ef5d2a0bbb
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-07-19 22:09:24 +0300

    fix bugs

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const URI = "mongodb+srv://oula:69NWds58r1S8qZ7R@firstcluster.woitwky.mongodb.net/imerologio";
+const dotenv = require("dotenv")
+dotenv.config();
+// const URI = "mongodb+srv://oula:69NWds58r1S8qZ7R@firstcluster.woitwky.mongodb.net/imerologio";
+const MONGO_URI = process.env.MONGO_URI;
 
 async function main(){
-    await mongoose.connect(URI);
+    await mongoose.connect(MONGO_URI);
 }
 
 main().then(()=>console.log("DB Connected!")).catch((err)=>console.log(err));
